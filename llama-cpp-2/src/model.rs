03085edeada70d5ff2c4ca80e457e014b435abd3
Sanzentyo <sanzen.tyo@gmail.com>
2025-09-14 02:08:47 +0900

    refactor: update lifetime annotations for context and kv_overrides methods

--- llama-cpp-2/src/model.rs
+++ llama-cpp-2/src/model.rs
@@ -667,10 +667,10 @@ impl LlamaModel {
     // we intentionally do not derive Copy on `LlamaContextParams` to allow llama.cpp to change the type to be non-trivially copyable.
     #[allow(clippy::needless_pass_by_value)]
     pub fn new_context(
-        &self,
+        &'_ self,
         _: &LlamaBackend,
         params: LlamaContextParams,
-    ) -> Result<LlamaContext, LlamaContextLoadError> {
+    ) -> Result<LlamaContext<'_>, LlamaContextLoadError> {
         let context_params = params.context_params;
         let context = unsafe {
             llama_cpp_sys_2::llama_new_context_with_model(self.model.as_ptr(), context_params)
